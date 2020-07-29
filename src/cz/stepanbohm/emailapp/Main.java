9da6b641d89dbb8c714397c60416c665112d512d
StepanB <stepan.bohm@expertkom.cz>
2020-07-29 22:22:05 +0200

    Finished.

--- src/cz/stepanbohm/emailapp/Main.java
+++ src/cz/stepanbohm/emailapp/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Email em1 = new Email("John", "Doe");
+        System.out.println(em1.toString());
+
+        em1.setAlternateEmail("js@gmail.com");
+        System.out.println(em1.toString());
     }
 }
